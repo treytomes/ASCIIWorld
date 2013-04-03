7cb8039508e3a31a24fb87cef846c4954728002c
Trey Tomes <trey.tomes@gmail.com>
2013-04-03 15:33:30 -0500

    Can now convert integer types.

--- ASCIIWorld/src/asciiWorld/Convert.java
+++ ASCIIWorld/src/asciiWorld/Convert.java
@@ -2,8 +2,11 @@ package asciiWorld;
 
 public class Convert {
 	public static <T> T changeType(Object obj, Class<T> type) throws Exception {
+		String value = obj.toString();
 		if (type.isAssignableFrom(String.class)) {
-			return type.cast(obj.toString());
+			return type.cast(value);
+		} else if (type.isAssignableFrom(Integer.class)) {
+			return type.cast(Integer.parseInt(value));
 		}
 		throw new Exception(String.format("I do not understand this type: %s", type.getName()));
 	}
