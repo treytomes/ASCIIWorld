c98b1c1eed486d65bacbd9d43208d647cbc88853
Trey Tomes <trey.tomes@gmail.com>
2013-06-28 11:38:43 -0500

    Added pink.

--- ASCIIWorld/src/asciiWorld/ColorFactory.java
+++ ASCIIWorld/src/asciiWorld/ColorFactory.java
@@ -8,4 +8,5 @@ public class ColorFactory {
 	public static final Color darkSlateGray = new Color(47, 79, 79, 255);
 	public static final Color orange = new Color(255, 165, 0, 255); // FFFFA500
 	public static final Color brown = new Color(165, 42, 42, 255); // FFA52A2A
+	public static final Color pink = new Color(255, 192, 203, 255); // FFFFC0CB
 }
